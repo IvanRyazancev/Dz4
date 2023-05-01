a99af5cd72950c6d953498556cc3212ee98f7a12
ryazancev.azov <ryazancev.azov@gmail.com>
2023-05-01 21:11:51 +0300

    Начало выполнения задачи №1

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> ls = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(reverse(ls));
+        ex1();
+    }
+
+
+    static void ex1() {
+        /*
+        Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+        Постараться не обращаться к листу по индексам.
+         */
+
+    }
+
+    public static LinkedList<Integer> reverse(LinkedList<Integer> ls) {
+        LinkedList<Integer> rev_ls = new LinkedList<>();
+        for (int i = ls.size() - 1; i >= 0; i--) {
+            rev_ls.add(ls.get(i));
+        }
+        return rev_ls;
+
+
+    }
+}
